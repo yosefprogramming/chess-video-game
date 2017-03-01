a5767132ddce429cc3c2bdc6061e2f13289f1323
David Yomtobian <david@yomtobian.com>
2017-02-28 20:41:24 -0500

    Add server logging with morgan. Add the fetch polyfill. Add a /bestmove route for requesting analysis on a fen. Request analysis onDrop (move the bestmove).

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const { join } = require("path")
+const morgan = require("morgan")
 const express = require("express")
 const app = express()
 
+app.use(morgan('dev'))
+
 app.use(express.static(join(__dirname, "static")))
+
+app.get('/bestmove/:fen', (req, res) => {
+
+  console.log('The browser is asking me to analyze', req.params.fen)
+  res.json({ bestmove: 'e7e6' })
+})
 
 app.listen(8000, () => {
   console.log("I'm listening")
