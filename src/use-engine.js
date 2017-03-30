084403adb60a631b07edd64353bb9e57684cbc2b
Allon Hadaya <self@allon.nyc>
2017-03-30 17:24:28 -0400

    Add alpha code that detects significant positions in a game.

--- src/use-engine.js
+++ src/use-engine.js
@@ -44,8 +44,31 @@ const evaluateFen = fen => engines
 
 const evaluateFens = fens => Promise.all(fens.map(evaluateFen));
 
+const normalizedScores = evaluations => evaluations.map((e, i) => {
+  const score = e.info[e.info.length - 1].score;
+  const value = score.value * (i % 2 ? 1 : -1);
+  return { unit: score.unit, value: value };
+});
 
-evaluateFens(pgnToFens(`[Event "Casual game"]
+const isSignificant = (a, b) => a.value - b.value > -125;
+
+const extractLevels = pgn => {
+  const fens = pgnToFens(pgn);
+  return evaluateFens(fens)
+    .then(evaluations => {
+      const scores = normalizedScores(evaluations.map(e => e.evaluation));
+      debugger;
+      const levels = [];
+      for (var i = 0; i < scores.length - 1; i += 2) {
+        if (isSignificant(scores[i], scores[i + 1])) {
+          levels.push(`${fens[i]} is a significant position: ${scores[i]} -> ${scores[i + 1]}`);
+        }
+      }
+      return levels;
+    });
+};
+
+extractLevels(`[Event "Casual game"]
 [Site "http://lichess.org/HI5OGtwB"]
 [Date "2016.03.19"]
 [White "Anonymous"]
@@ -60,8 +83,8 @@ evaluateFens(pgnToFens(`[Event "Casual game"]
 [Opening "Scandinavian Defense"]
 [Termination "Normal"]
 [Annotator "lichess.org"]
-1. e4 d5 { B01 Scandinavian Defense } 2. d3 e5 3. Nf3 Qd6 4. Bd2 Nf6 5. Bc3 Nc6 6. Nbd2 d4 7. Nc4 Qc5 8. Bd2 h6 9. Qe2 a5 10. O-O-O Be6 11. b3 O-O-O 12. a3 b6 13. b4 axb4 14. axb4 Qxb4 15. Bxb4 Bxb4 16. Nfxe5 Kb7 17. Kb2 Ra8 18. Kb3 Bc3 19. Nf3 b5 20. Nxd4 Nxd4+ 21. Kxc3 c5 22. Rb1 Nd5+ 23. exd5 Nxe2+ 24. Bxe2 Bd7 25. Nd6+ Kb6 26. Nc4+ Kb7 27. d4 cxd4+ 28. Kxd4 Ra6 29. h3 Rha8 30. Nd2 Ra5 31. Rb3 Re8 32. Bg4 f5 33. Bh5 Re7 34. Rhb1 Ra6 35. Bf3 g5 36. d6+ Kc8 37. dxe7 Rd6+ 38. Kc3 g4 39. Be2 Re6 40. Bxb5 gxh3 41. gxh3 Rxe7 42. Bxd7+ Kxd7 43. Rb7+ Ke8 44. Rxe7+ Kd8 45. Rbb7 Kc8 46. Nc4 Kd8 47. Nd6 f4 48. Re8# { Black is checkmated } 1-0`)).then(evaluations => {
-  console.log(evaluations);
+1. e4 d5 { B01 Scandinavian Defense } 2. d3 e5 3. Nf3 Qd6 4. Bd2 Nf6 5. Bc3 Nc6 6. Nbd2 d4 7. Nc4 Qc5 8. Bd2 h6 9. Qe2 a5 10. O-O-O Be6 11. b3 O-O-O 12. a3 b6 13. b4 axb4 14. axb4 Qxb4 15. Bxb4 Bxb4 16. Nfxe5 Kb7 17. Kb2 Ra8 18. Kb3 Bc3 19. Nf3 b5`).then(levels => {
+  console.log(levels);
   clearInterval(pendingId);
   debugger;
 })
