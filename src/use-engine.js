700c7b4c19fc7ef4cc82d344adceadc43d1fd4c2
David Yomtobian <david@yomtobian.com>
2017-04-04 20:56:23 -0400

    Evaluate pgns from the filesystem.

--- src/use-engine.js
+++ src/use-engine.js
@@ -2,7 +2,7 @@ const engines = require('./engine-pool');
 const { Chess } = require('chess.js');
 
 const pendingId = setInterval(function () {
-  console.log(engines.pending);
+  console.error(engines.pending);
 }, 1000);
 
 
@@ -60,7 +60,6 @@ const extractLevels = pgn => {
   return evaluateFens(fens)
     .then(evaluations => {
       const scores = normalizedScores(evaluations.map(e => e.evaluation));
-      debugger;
       const levels = [];
       for (var i = 0; i < scores.length - 1; i += 2) {
         if (isSignificant(scores[i], scores[i + 1], 175)) {
@@ -73,23 +72,14 @@ const extractLevels = pgn => {
     });
 };
 
-extractLevels(`[Event "Casual game"]
-[Site "http://lichess.org/HI5OGtwB"]
-[Date "2016.03.19"]
-[White "Anonymous"]
-[Black "lichess AI level 1"]
-[Result "1-0"]
-[WhiteElo "?"]
-[BlackElo "?"]
-[PlyCount "95"]
-[Variant "Standard"]
-[TimeControl "-"]
-[ECO "B01"]
-[Opening "Scandinavian Defense"]
-[Termination "Normal"]
-[Annotator "lichess.org"]
-1. e4 d5 { B01 Scandinavian Defense } 2. d3 e5 3. Nf3 Qd6 4. Bd2 Nf6 5. Bc3 Nc6 6. Nbd2 d4 7. Nc4 Qc5 8. Bd2 h6 9. Qe2 a5 10. O-O-O Be6 11. b3 O-O-O 12. a3 b6 13. b4 axb4 14. axb4 Qxb4 15. Bxb4 Bxb4 16. Nfxe5 Kb7 17. Kb2 Ra8 18. Kb3 Bc3 19. Nf3 b5`).then(levels => {
+const pgnPath = process.argv[2];
+
+const fs = require('fs');
+const pgn = fs.readFileSync(pgnPath, "utf-8");
+
+console.log(pgn)
+
+extractLevels(pgn).then(levels => {
   console.log(levels);
   clearInterval(pendingId);
-  debugger;
-})
+});
